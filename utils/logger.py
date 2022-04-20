e3e26cff5e7e8a9ad10560a71eaf81b9eca7cb57
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-04-20 19:24:57 +0530

    Remove `.` appendations

--- utils/logger.py
+++ utils/logger.py
@@ -4,19 +4,19 @@ log_path = "./Desktop/Stock/logs/info-log.txt"
 error_path = "./Desktop/Stock/logs/info-log.txt"
 def info(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    f.write(f'[{current_time}/INFO] {text}.')
+    f.write(f'[{current_time}/INFO] {text}')
     with open(log_path, 'a') as f:
-        f.write(f'[{current_time}/INFO] {text}.\n')
+        f.write(f'[{current_time}/INFO] {text}\n')
         f.close()
 def warn(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    f.write(f'[{current_time}/WARN] {text}.')
+    f.write(f'[{current_time}/WARN] {text}')
     with open(log_path, 'a') as f:
-        f.write(f'[{current_time}/WARN] {text}.\n')
+        f.write(f'[{current_time}/WARN] {text}\n')
         f.close()
 def error(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    f.write(f'[{current_time}/ERROR] {text}.')
+    f.write(f'[{current_time}/ERROR] {text}')
     with open(error_path, 'a') as f:
-        f.write(f'[{current_time}/ERROR] {text}.\n')
+        f.write(f'[{current_time}/ERROR] {text}\n')
         f.close()
