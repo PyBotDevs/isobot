0c53da29fe9313c1ee766d082b1a759b6edee888
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-04-20 19:20:54 +0530

    Log stuff to console
    ### logger library update
    This will force any info logs, warn logs, or error logs to initially be spooled to the working console.
    This sets the log file paths as second priority, allowing those to soft-fail.

--- utils/logger.py
+++ utils/logger.py
@@ -1,19 +1,22 @@
 import time
 import datetime
-log_path = "./Desktop/Stock/logs/info-log.txt"
-error_path = "./Desktop/Stock/logs/info-log.txt"
+log_path = "/home/notsniped/Desktop/Stock/logs/info-log.txt"
+error_path = "/home/notsniped/Desktop/Stock/logs/info-log.txt"
 def info(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    f.write(f'[{current_time}/INFO] {text}.')
     with open(log_path, 'a') as f:
         f.write(f'[{current_time}/INFO] {text}.\n')
         f.close()
 def warn(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    f.write(f'[{current_time}/WARN] {text}.')
     with open(log_path, 'a') as f:
         f.write(f'[{current_time}/WARN] {text}.\n')
         f.close()
 def error(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    f.write(f'[{current_time}/ERROR] {text}.')
     with open(error_path, 'a') as f:
         f.write(f'[{current_time}/ERROR] {text}.\n')
         f.close()
