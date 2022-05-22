834cbb8ac7b7d297cd7af55a45f5d662a28e0a21
snipe <72265661+notsniped@users.noreply.github.com>
2022-05-22 13:38:13 -0400

    Fix logger not printing errors to console

--- utils/logger.py
+++ utils/logger.py
@@ -4,19 +4,19 @@ log_path = "./Desktop/Stock/logs/info-log.txt"
 error_path = "./Desktop/Stock/logs/error-log.txt"
 def info(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    f.write(f'[{current_time}/INFO] {text}')
+    print(f'[{current_time}/INFO] {text}')
     with open(log_path, 'a') as f:
         f.write(f'[{current_time}/INFO] {text}\n')
         f.close()
 def warn(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    f.write(f'[{current_time}/WARN] {text}')
+    print(f'[{current_time}/WARN] {text}')
     with open(log_path, 'a') as f:
         f.write(f'[{current_time}/WARN] {text}\n')
         f.close()
 def error(text:str):
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    f.write(f'[{current_time}/ERROR] {text}')
+    print(f'[{current_time}/ERROR] {text}')
     with open(error_path, 'a') as f:
         f.write(f'[{current_time}/ERROR] {text}\n')
         f.close()
