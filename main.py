43b57426a601aec58522584a9b784658c840c62f
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-07-31 10:24:15 +0530

    Floor AFK UNIX timestamp value
    The value isn't rounded in the AFK database, so it will have to be rounded while being outputted for Discord to read it.

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ async def on_message(ctx):
     for i in uList:
         if i in ctx.content and not message.author.bot:
             user = client.get_user(i)
-            ctx.channel.send(f"{user.display_name} went AFK {user_presence[str(ctx.guild.id)][str(ctx.author.id)]['exctime']}: {user_presence[str(ctx.guild.id)][str(ctx.author.id)]['response']}")
+            ctx.channel.send(f"{user.display_name} went AFK {math.floor(user_presence[str(ctx.guild.id)][str(ctx.author.id)]['exctime'])}: {user_presence[str(ctx.guild.id)][str(ctx.author.id)]['response']}")
     if str(ctx.author.id) in user_presence[str(ctx.guild.id)]:
         del user_presence[str(ctx.guild.id)][str(ctx.author.id)]
         save()
