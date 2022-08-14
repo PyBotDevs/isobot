23c3ce4a10a439d316ceb99d9e55b13ccc2f179f
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-08-14 10:11:31 +0530

    Make EmbedEngine in NKA Framework

---
+++ framework/isobot/embedengine.py
@@ -0,0 +1,33 @@
+"""The library used for designing and outputting Discord embeds."""
+import discord
+from discord import Color
+
+
+def embed(title: str, desc: str, *, image: str = None, thumbnail: str = None, color=None, footer: dict = None):
+    """Designs a Discord embed.
+    -----------------------------------------------------------
+    The color argument is completely optional.
+    - If a color is set, it will return the embed in that color only.
+    - If the color is set as "rand", then it will return the embed with a random color.
+    - If a color is not set, it will appear as the Discord client's default embed color.
+
+    Footer must be in a json format ONLY, otherwise it cannot be parsed.
+    - Correct format: {"text": "something", "img" "an image url"}
+    """
+    if color == "rand":
+        color = Color.random()
+    local_embed = discord.Embed()
+    local_embed.title(title)
+    local_embed.description(desc)
+    if image is not None:
+        local_embed.set_image(url=image)
+    if thumbnail is not None:
+        local_embed.set_thumbnail(url=thumbnail)
+    if color is not None:
+        local_embed.colour(color)
+    if footer is not None:
+        try:
+            local_embed.set_footer(text=footer["text"], icon_url=footer["img"])
+        except KeyError:
+            return "Unable to create embed: Failed to parse \"footer\" argument. Expected format: {\"text\": \"something\", \"img\" \"an image url\"}"
+    return local_embed
