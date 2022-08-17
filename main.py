f52ef99c00a8705b5e5d7d25d4d49212c56bdfc6
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-08-17 20:51:43 +0530

    Debloat logic further

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ async def deposit(ctx:SlashContext, amount):
     if plugins.economy:
         if not amount.isnumeric():
             if amount == "max": amount = currency["wallet"][str(ctx.author.id)]
-            if amount != "max": return await ctx.reply("The amount must be a number, or `max`.", hidden=True)
+            else: return await ctx.reply("The amount must be a number, or `max`.", hidden=True)
         elif currency['bank'] == 0: return await ctx.reply('You don\'t have anything in your bank account.', hidden=True)
         elif int(amount) <= 0: return await ctx.reply('The amount to deposit must be more than `0` coins!', hidden=True)
         elif int(amount) > currency["wallet"][str(ctx.author.id)]: return await ctx.reply('The amount to deposit must not be more than what you have in your wallet!', hidden=True)
