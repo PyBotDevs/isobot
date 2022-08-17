692ec621850eee1d245af91904e3ba078a516ec2
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-08-17 20:12:46 +0530

    Set "max" and "all" as keywords for max amount in `/withdraw`

--- main.py
+++ main.py
@@ -281,12 +281,12 @@ async def deposit(ctx:SlashContext, amount):
     name='withdraw',
     description='Withdraws a specified amount of cash from the bank.',
     options=[
-        create_option(name='amount', description='Specify an amount to withdraw (leave blank for max)', option_type=4, required=False)
+        create_option(name='amount', description='Specify an amount to withdraw (use max for everything)', option_type=4, required=True)
     ]
 )
-async def withdraw(ctx:SlashContext, amount=None):
+async def withdraw(ctx:SlashContext, amount):
     if plugins.economy:
-        if amount == None: amount = currency["bank"][str(ctx.author.id)]
+        if amount in ["all", "max"]: amount = currency["wallet"][str(ctx.author.id)]
         elif currency['bank'] == 0: return await ctx.reply('You don\'t have anything in your bank account.', hidden=True)
         elif amount <= 0: return await ctx.reply('The amount to withdraw must be more than `0` coins!', hidden=True)
         elif amount > currency["bank"][str(ctx.author.id)]: return await ctx.reply('The amount to withdraw must not be more than what you have in your bank account!', hidden=True)
