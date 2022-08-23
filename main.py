d7ef4b3efd5eca4f01b7c77494ed7f6d4112bc3a
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-08-23 20:14:54 +0530

    Make list names more understandable

--- main.py
+++ main.py
@@ -642,9 +642,9 @@ async def open(ctx:SlashContext, lootbox:str, amount:int):
     types = ["normal", "large", "special"]
     if amount <= 0: return await ctx.reply("You can't open 0 or below lootboxes! Don't be stupid.", hidden=True)
     elif lootbox not in types: return await ctx.reply("That lootbox doesn't even exist get a brain", hidden=True)
-    n_loot = [random.randint(10000, 25000)]
-    l_loot = [random.randint(50000, 75000)]
-    s_loot = [random.randint(100000, 500000)]
+    normal_loot = [random.randint(10000, 25000)]
+    large_loot = [random.randint(50000, 75000)]
+    special_loot = [random.randint(100000, 500000)]
     # ^ Make item rewards here
 
 @slash.slash(
