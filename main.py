1007aed5e3b6ccfcd5bb9002e0ae584c9f9b3a4a
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-09-07 20:31:50 +0530

    Disable NKA framework's currency manager temporarily

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class plugins:
     music = False
 
 colors = framework.isobot.colors.Colors()
-currency_unused = framework.isobot.currency.CurrencyAPI(f'{wdir}/database/currency.json', f"{wdir}/logs/currency.log")  # Initialize part of the framework (Currency)
+# currency_unused = framework.isobot.currency.CurrencyAPI(f'{wdir}/database/currency.json', f"{wdir}/logs/currency.log")  # Initialize part of the framework (Currency)
 # isobank = framework.isobank.manager.IsoBankManager(f"{wdir}/database/isobank/accounts.json", f"{wdir}/database/isobank/auth.json")
 isobankauth = framework.isobank.authorize.IsobankAuth(f"{wdir}/database/isobank/auth.json", f"{wdir}/database/isobank/accounts.json")
 
