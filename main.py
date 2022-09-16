e40ce349c9ee099eeff722099ca1163285bd3c9c
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-09-16 22:25:23 +0530

    Explicitly define `/automod_swearfilter` command usage to server administrators only

--- main.py
+++ main.py
@@ -1249,6 +1249,7 @@ async def automod(ctx:SlashContext):
         create_option(name="toggle", description="Do you want to turn it on or off?", option_type=5, required=True)
     ]
 )
+@commands.has_permissions("administrator")
 async def automod_swearfilter(ctx:SlashContext, toggle:bool):
     if loaded_config["swear_filter"]["enabled"] == toggle: return await ctx.reply(f"That automod option is already set to `{toggle}`.", hidden=True)
     loaded_config["swear_filter"]["enabled"] = toggle
