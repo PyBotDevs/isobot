095135f7439a81dea787a260e116b28abc583e7d
snipe <72265661+notsniped@users.noreply.github.com>
2022-12-04 12:51:13 +0530

    Add footers to embed responses in `/squareroot` command

--- main.py
+++ main.py
@@ -1304,9 +1304,13 @@ async def automod_remove_custom_keyword(ctx: ApplicationContext, id:int):
 )
 @option(name="number", description="Which number do you want to find the root of?", type=int)
 async def squareroot(ctx: ApplicationContext, number: int):
-    if number < 0: return await ctx.respond("The square root of a negative number is an imaginary number.")
+    if number < 0: 
+        localembed = discord.Embed(description="The square root of a negative number is an imaginary number.", color=color)
+        localembed.set_footer(text=f"√({number}) = i√{number}")
+        return await ctx.respond(embed=localembed)
     result = sqrt(number)
     localembed = discord.Embed(title=f"Square root of {number}", description=result, color=color)
+    localembed.set_footer(text=f"√({number}) = {result}")
     await ctx.respond(embed=localembed)
 
 # Initialization
