42adb72af0ffe053d9f4f30f653797df35245e3c
snipe <72265661+notsniped@users.noreply.github.com>
2023-03-31 20:48:30 +0530

    Add back `/kick` and `/ban` commands

--- main.py
+++ main.py
@@ -224,6 +224,38 @@ async def sync(ctx: ApplicationContext):
         print(e)
         await ctx.respond('An error occured while resyncing. Check console.', ephemeral=True)
 
+@client.slash_command(
+    name='kick', 
+    description='Kicks a member from this server.'
+)
+@option(name="user", description="Who do you want to kick?", type=discord.User)
+@option(name="reason", description="Why do you want to kick the user?", type=str, default=None)
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def kick(ctx: ApplicationContext, user, reason=None):
+    if not ctx.author.guild_permissions.kick_members: return await ctx.respond('https://tenor.com/view/oh-yeah-high-kick-take-down-fight-gif-14272509')
+    else:
+        try:
+            if reason is None: await user.kick()
+            else: await user.kick(reason=reason)
+            await ctx.respond(embed=discord.Embed(title=f'{user} has been kicked.', description=f'Reason: {str(reason)}'))
+        except Exception: await ctx.respond(embed=discord.Embed(title='Well, something happened...', description='Either I don\'t have permission to do this, or my role isn\'t high enough.', color=discord.Colour.red()))
+
+@client.slash_command(
+    name='ban', 
+    description='Bans a member from this server.'
+)
+@option(name="user", description="Who do you want to ban?", type=discord.User)
+@option(name="reason", description="Why you want to ban the user?", type=str, default=None)
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def ban(ctx: ApplicationContext, user, reason=None):
+    if not ctx.author.guild_permissions.ban_members: return await ctx.respond('https://tenor.com/view/thor-strike-admin-ban-admin-ban-gif-22545175')
+    else:
+        try:
+            if reason is None: await user.ban()
+            else: await user.ban(reason=reason)
+            await ctx.respond(embed=discord.Embed(title=f'{user} has been banned.', description=f'Reason: {str(reason)}'))
+        except Exception: await ctx.respond(embed=discord.Embed(title='Well, something happened...', description='Either I don\'t have permission to do this, or my role isn\'t high enough.', color=discord.Colour.red()))
+
 # Cog Commands (these cannot be moved into a cog)
 cogs = client.create_group("cog", "Commands for working with isobot cogs.")
 
