716e46cb425f0288e8a643425470753631250af0
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2023-05-29 23:04:19 +0530

    Remove old unused currency commands from economy cog

--- cogs/economy.py
+++ cogs/economy.py
@@ -46,47 +46,17 @@ jobs = [
     "Doctor"
 ]
 
-with open(f"{wdir}/database/currency.json", 'r') as f: currency = json.load(f)
+# with open(f"{wdir}/database/currency.json", 'r') as f: currency = json.load(f)
 with open(f"{wdir}/database/items.json", 'r') as f: items = json.load(f)
 with open(f"{wdir}/config/shop.json", 'r') as f: shopitem = json.load(f)
 with open(f"{wdir}/database/user_data.json", 'r') as f: userdat = json.load(f)
 
 def save():
-    with open(f"{wdir}/database/currency.json", 'w+') as f: json.dump(currency, f, indent=4)
+    # with open(f"{wdir}/database/currency.json", 'w+') as f: json.dump(currency, f, indent=4)
     with open(f"{wdir}/database/items.json", 'w+') as f: json.dump(items, f, indent=4)
     with open(f"{wdir}/database/user_data.json", 'w+') as f: json.dump(userdat, f, indent=4)
 
 # Functions
-def get_user_networth(user_id:int):
-    nw = currency["wallet"][str(user_id)] + currency["bank"][str(user_id)]
-    #for e in items[str(user_id)]:
-    #    if e != 0: nw += shopitem[e]["sell price"]
-    return nw
-
-def get_wallet(id: int) -> int:
-    return currency['wallet'][str(id)]
-
-def get_bank(id: int) -> int:
-    return currency['bank'][str(id)]
-
-def new_wallet(id: int):
-    if str(id) not in currency['wallet']: 
-        currency['wallet'][str(id)] = 5000
-        return 0
-    else: return 1
-    
-def new_bank(id: int):
-    if str(id) not in currency['bank']: 
-        currency['bank'][str(id)] = 0
-        return 0
-    else: return 1
-
-def get_user_count():
-    users = 0
-    for x in currency["wallet"].keys():
-        users += 1
-    return users
-
 def new_userdat(id: int):
     if str(id) not in userdat.keys(): 
         userdat[str(id)] = {"work_job": None}
