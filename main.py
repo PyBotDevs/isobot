3221ec6671c0f7d1bb283873628ea71508b9f586
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2023-06-08 22:56:11 +0530

    Reduce set delay between license display and startup info

--- main.py
+++ main.py
@@ -85,9 +85,9 @@ This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions.
 __________________________________________________""")
-    time.sleep(2)
     print(f'Logged in as {client.user.name}.')
     print('Ready to accept commands.')
+    time.sleep(1)
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="GitHub"), status=discord.Status.idle)
     print(f'[main/LOG] {colors.green}Status set to IDLE. Rich presence set.{colors.end}')
     print("[main/FLASK] Starting pinger service...")
