c47f1db8fd39262d2192167a2978f6459a832a5d
Sabi <120003982+cyanogus@users.noreply.github.com>
2023-07-06 08:48:08 +0000

    Rename internal redefined `help()` command

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ async def on_application_command_error(ctx: ApplicationContext, error: discord.D
     description="Gives you help with a specific command, or shows a list of all commands"
 )
 @option(name="command", description="Which command do you need help with?", type=str, default=None)
-async def help(ctx: ApplicationContext, command: str = None):
+async def _help(ctx: ApplicationContext, command: str = None):
     """Gives you help with a specific command, or shows a list of all commands"""
     if command is not None:
         try:
