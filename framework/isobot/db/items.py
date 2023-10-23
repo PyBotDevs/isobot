0009cf22269eb21bb6760ea3726a451c7046a99b
snipe <72265661+notsniped@users.noreply.github.com>
2023-10-23 15:51:17 +0530

    Add missing data parameter to `save()`

--- framework/isobot/db/items.py
+++ framework/isobot/db/items.py
@@ -37,14 +37,14 @@ class Items():
         """Adds an item of choice to a specific user id."""
         items = self.load()
         items[str(user_id)][item] += quantity
-        self.save()
+        self.save(items)
         return 0
 
     def remove_item(self, user_id: int, item: str, *, quantity: int = 1) -> int:
         """Removes an item of choice from a specific user id."""
         items = self.load()
         items[str(user_id)][item] += quantity
-        self.save()
+        self.save(items)
         return 0
 
     def fetch_item_count(self, user_id: int, item: str) -> int:
