db297351bafb55fda32c0144c2a9438bc17449b0
snipe <72265661+notsniped@users.noreply.github.com>
2024-03-21 17:45:34 +0530

    Fix incorrect argument in `discord.Embed`

--- cogs/economy.py
+++ cogs/economy.py
@@ -556,7 +556,7 @@ class Economy(commands.Cog):
         if user == None: user = ctx.author
         try:
             ntw = currency.get_user_networth(user.id)
-            localembed = discord.Embed(name=f"{user.display_name}'s networth", description=f"{ntw} coins", color=color)
+            localembed = discord.Embed(title=f"{user.display_name}'s networth", description=f"{ntw} coins", color=color)
             await ctx.respond(embed=localembed)
         except KeyError: return await ctx.respond("Looks like that user isn't cached yet. Please try again later.", ephemeral=True)
 
