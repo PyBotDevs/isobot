9a03a4f6e243dbf7bc19ceaa2715fe4cf1365c65
snipe <72265661+notsniped@users.noreply.github.com>
2024-03-22 11:03:28 +0530

    Improve logging system for `logger.info()`

--- utils/logger.py
+++ utils/logger.py
@@ -9,9 +9,17 @@ class colours:
     success = '\033[92m'
     warning = '\033[33m'
     end = '\033[0m'
-def info(text:str, *, nolog=False):
+
+# Main Functions
+def info(text:str, *, nolog=False, module=None, timestamp=False):
+    """Logs a client information alert to the console and the log file.\n`nolog=True` option skips logging to file."""
     current_time = datetime.datetime.now().strftime("%H:%M:%S")
-    print(f'[{current_time}/INFO] {text}')
+    if module is not None:
+        if not timestamp: print(f'[{module}] {text}')
+        else: print(f'[{current_time}] [{module}] {text}')
+    else:
+        if not timestamp: print(text)
+        else: print(f'[{current_time}] {text}')
     if nolog == True: pass
     else:
       with open(log_path, 'a') as f:
