5557262c6f5be5deb0799b89dc9752fa69bda070
snipe <72265661+notsniped@users.noreply.github.com>
2024-03-22 11:05:50 +0530

    Add new class `logger.StartupLog()` for logging client startup logs

--- utils/logger.py
+++ utils/logger.py
@@ -51,3 +51,34 @@ def error(text:str, *, nolog=False, module=None):
           if module is not None: f.write(f'[{current_time}] [{module}] ERROR: {text}\n')
           else: f.write(f'[{current_time}] ERROR: {text}\n')
           f.close()
+
+# Startup Log Class
+class StartupLog():
+    def __init__(self, log_path: str, *, add_timestamps: bool = False, clear_old_logs: True):
+        """Used to log the isobot client's startup logs."""
+        self.log_path = log_path
+        self.add_timestamps = add_timestamps
+        if clear_old_logs:
+            with open(log_path, 'w+', encoding="utf-8") as f:
+                f.write("# Startup log will be shown here!\n")
+                f.close()
+
+    def clean(self, data: str):
+        """Cleans out any extra metadata (like colour codes) from the data."""
+        color_codes = ['\033[91m', '\033[92m', '\033[93m', '\033[33m', '\033[0m']
+        data_d = data  # Specify new isolated variable for performing string operations on
+        for x in color_codes:
+            if x in data_d:
+                data_d = data_d.replace(x, "")
+        return data_d
+
+    def log(self, data: str, *, print_log: bool = True):
+        """Writes a log to the specified log file."""
+        if print_log: print(data)
+        data = self.clean(data)
+        with open(self.log_path, 'a', encoding="utf-8") as f:
+            if self.add_timestamps:
+                current_time = datetime.datetime.now().strftime("%H:%M:%S")
+                f.write(f"[{current_time}] {data}\n")
+            else: f.write(f"{data}\n")
+            f.close()
