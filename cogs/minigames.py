c577b1bde58f9b5b07548969984a1d0c0d343d99
snipe <72265661+notsniped@users.noreply.github.com>
2024-04-02 13:12:20 +0530

    Switch from `commands.wait_for()` to `self.bot.wait_for()`

--- cogs/minigames.py
+++ cogs/minigames.py
@@ -23,16 +23,16 @@ class Minigames(commands.Cog):
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def guessthenumber(self, ctx: ApplicationContext):
         number = randint(1, 10)
-        localembed = discord.Embed(name="Guess the number!", description="I am currently thinking of a number from 1 to 10. Can you guess what it is?", color=color)
+        localembed = discord.Embed(title="Guess the number!", description="I am currently thinking of a number from 1 to 10. Can you guess what it is?", color=color)
         localembed.set_footer(text="If you guess what it is, you will win 500 to 1000 coins!")
         await ctx.respond(embed=localembed)
         def check(msg): return msg.author == ctx.author and msg.channel == ctx.channel and msg.content
-        msg = await commands.wait_for("message", check=check)
+        msg = await self.bot.wait_for("message", check=check)
         if int(msg.content) == number:
             randcoins = randint(500, 1000)
             currency.add(ctx.author.id, randcoins)
-            await ctx.respond(f"Correct! You've just won **{randcoins} coins** by guessing the correct number.")
-        else: return await ctx.respond("Too bad bozo, you guessed the number wrong and you won nothing.")
+            await ctx.channel.send(f"Correct {ctx.author.mention}! You've just won **{randcoins} coins** by guessing the correct number.")
+        else: return await ctx.channel.send(f"{ctx.author.mention} Too bad bozo, you guessed the number wrong and you won nothing.")
 
     @commands.slash_command(
         name="highlow",
