69d1e147a8d3e0766024ee7fb1b9c5334a2454a9
snipe <72265661+notsniped@users.noreply.github.com>
2024-04-08 11:05:16 +0530

    Explicitly define `user` as `str` type when performing uid match check in message

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ async def on_message(ctx):
                 for userid in presence[str(ctx.guild.id)].keys(): uList.append(userid)
             else: pass
             for user in uList:
-                if user in ctx.content and not ctx.author.bot:
+                if str(user) in ctx.content and not ctx.author.bot:
                     fetch_user = client.get_user(id(user))
                     await ctx.channel.send(f"{fetch_user.display_name} went AFK <t:{floor(presence[str(ctx.guild.id)][str(user)]['time'])}:R>: {presence[str(ctx.guild.id)][str(user)]['response']}")
             if str(ctx.guild.id) in presence and str(ctx.author.id) in presence[str(ctx.guild.id)]:
