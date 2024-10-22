82e25a654cf622fefbe95455e766384688eb49d4
snipe <72265661+notsniped@users.noreply.github.com>
2024-10-22 19:02:05 +0530

    Create embeds database management module in framework

---
+++ framework/isobot/db/embeds.py
@@ -0,0 +1,19 @@
+# Imports
+import json
+from framework.isobot.colors import Colors as colors
+
+# Functions
+class Automod():
+    """Initializes the EmbedEngine database system."""
+    def __init__(self):
+        print(f"[framework/db/Automod] {colors.green}Embeds db library initialized.{colors.end}")
+
+    def load(self) -> dict:
+        """Fetches and returns the latest data from the items database."""
+        with open("database/embeds.json", 'r', encoding="utf8") as f: db = json.load(f)
+        return db
+
+    def save(self, data: dict) -> int:
+        """Dumps all cached data to your local machine."""
+        with open("database/embeds.json", 'w+', encoding="utf8") as f: json.dump(data, f)
+        return 0
