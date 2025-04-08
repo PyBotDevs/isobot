5705104e710102ea570ad4c9345807170199e15b
snipe <72265661+notsniped@users.noreply.github.com>
2025-04-08 22:33:39 +0530

    Remove all local CommandsDB management commands
    This is done in favour of the new isobot client update, which moves all of the configuration files (including the commands db) to the resources repository, and allows for automatic updating of broken, modified or outdated configuration databases.

--- framework/isobot/commands.py
+++ framework/isobot/commands.py
@@ -17,11 +17,6 @@ class Commands():
         with open(f"{client_data_dir}/config/commands.json", 'r', encoding="utf-8") as f: data = json.load(f)
         return data
 
-    def save(self, data: dict) -> int:
-        """Saves the cached database to local machine storage."""
-        with open(f"{client_data_dir}/config/commands.json", 'w+', encoding="utf-8") as f: json.dump(data, f)
-        return 0
-
     def fetch_raw(self) -> dict:
         """Returns the commands database in a raw `json` format."""
         return self.load()
@@ -30,40 +25,3 @@ class Commands():
         """Returns all the added commands as a `list`."""
         cmds = self.load()
         return cmds.keys()
-
-    def command_disabled_flag(self, command: str, status: bool):
-        """Enables or disables the `disabled` flag for a command."""
-        cmds = self.load()
-        cmds[command]["disabled"] = bool
-        self.save(cmds)
-        return 0
-
-    def command_bugged_flag(self, command: str, status: bool):
-        """Enables or disables the `bugged` flag for a command."""
-        cmds = self.load()
-        cmds[command]["bugged"] = bool
-        self.save(cmds)
-        return 0
-
-    def add_command(self, command_name: str, stylized_name: str, description: str, command_type: str, usable_by: str, *, cooldown: int = None):
-        """Adds a new command entry into the command registry."""
-        cmds = self.load()
-        cmds[command_name] = {
-            "name": stylized_name,
-            "description": description,
-            "type": command_type,
-            "cooldown": cooldown,
-            "args": None,
-            "usable_by": usable_by,
-            "disabled": False,
-            "bugged": False
-        }
-        self.save(cmds)
-        return 0
-
-    def remove_command(self, command):
-        """Removes a command permanently from the command registry."""
-        cmds = self.load()
-        del cmds[command]
-        self.save(cmds)
-        return 0
